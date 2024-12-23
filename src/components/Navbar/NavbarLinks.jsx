7bc1bdf4a9195d35556d99032755abf790131558
GhuniNew <aakanun43@gmail.com>
2024-12-23 15:48:07 +0700

    update link to www

--- src/components/Navbar/NavbarLinks.jsx
+++ src/components/Navbar/NavbarLinks.jsx
@@ -8,6 +8,7 @@ function NavbarLinks({ link, ...rest }) {
             {...rest}
             to={link.to || "/"}
             end={link.to === "/" ? true : false}
+            target={link.to === "http://www.lantakayaking.com" && "_blank"}
             className={({ isActive }) =>
                 cx(
                     "z-10 block w-full rounded-md px-3 py-2 hover:bg-orange-600 hover:text-white md:w-auto",
